e6166dab9cd23cd1ca41c09a0dc65704b59a3482
yqc <yqc1236@qq.com>
2024-10-13 00:01:05 +0800

    苍穹外卖 公共字段自动填充

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -42,7 +42,7 @@ public class AutoFillAspect {
 
         //获取当前被拦截的方法上的数据库操作类型(UPDATE 还是 INSERT)
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();//方法签名对象
-        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);//获取方法上的注解对象
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);//获取方法上的注解对象(加上要获取的注解的.class)
         OperationType operationType = autoFill.value();//获得数据库操作类型
 
         //获取当前被拦截的方法参数(实体对象)
@@ -51,7 +51,7 @@ public class AutoFillAspect {
         if (args == null || args.length == 0) {
             return;
         }
-        Object entity = args[0];//(entity是Object类型,没有getset方法,想给里面的东西赋值得用反射)
+        Object entity = args[0];//(entity是Object类型的实体类对象,没有getset方法,想给里面的东西赋值得用反射)
 
         //准备赋值的数据(当前操作人id 和 当前时间)
         LocalDateTime now = LocalDateTime.now();
@@ -60,10 +60,10 @@ public class AutoFillAspect {
         //根据不同的操作类型,为对应的属性来赋值(通过反射)
         if (operationType == OperationType.INSERT){//反射三要素 : 调用什么对象 形参是什么  返回什么
 
-            //为4个公共字段赋值 (.getClass()获取该实体对象的class文件)
-            //                (.getDeclaredMethod()从此clss文件中获取一个(Declared无论是公共还是私有都可获取)方法的对象
-            //                "setCreateTime"获取的方法对象的名字    LocalDateTime.class此方法的返回值类型的class
-
+            //为4个公共字段赋值 (实体类对象.getClass()获取该实体对象的class文件)
+            //                (.getDeclaredMethod()从此clss文件中调用一个(Declared无论是公共还是私有都可获取)方法的对象        ---调用什么对象
+            //                "setCreateTime"获取的方法对象的名字    LocalDateTime.class此方法的形参类型的.class形式    ---形参是什么
+            //                 这几个set方法都没有返回值,所以不用写     ---调用什么对象
 
             try {//"setCreateTime"已经被封装为常量类AutoFillConstant中
             Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
