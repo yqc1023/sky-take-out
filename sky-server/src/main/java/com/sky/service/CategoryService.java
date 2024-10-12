dea7e569bf73b31771833d406953483d13289367
yqc <yqc1236@qq.com>
2024-10-12 12:56:09 +0800

    苍穹外卖 分类管理所有代码

--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -2,10 +2,11 @@ package com.sky.service;
 
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
 import com.sky.result.PageResult;
+import java.util.List;
 
 public interface CategoryService {
-
 
     /**
      * 新增分类
@@ -14,21 +15,35 @@ public interface CategoryService {
     void save(CategoryDTO categoryDTO);
 
     /**
-     * 分类查询菜品
+     * 分页查询
      * @param categoryPageQueryDTO
      * @return
      */
     PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
 
     /**
-     * 启用和禁用分类
-     * @param status
+     * 根据id删除分类
+     * @param id
      */
-    void startOrStop(Integer status,Long id);
+    void deleteById(Long id);
 
     /**
      * 修改分类
      * @param categoryDTO
      */
     void update(CategoryDTO categoryDTO);
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    List<Category> list(Integer type);
 }
