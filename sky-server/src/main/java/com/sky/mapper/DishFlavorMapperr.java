e2e02bb3269dbef980c3add260984e6c2100530c
yqc <yqc1236@qq.com>
2024-10-16 20:01:43 +0800

    苍穹外卖 修改菜品信息代码及对应口味信息(口味部分)代码

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapperr.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapperr.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -20,5 +21,20 @@ public interface DishFlavorMapperr {
      * 批量删除口味
      * @param dishIds
      */
-    void delete(List<Long> dishIds);
+    void deleteByIds(List<Long> dishIds);
+
+    /**
+     * 修改菜品口味
+     *
+     * @param
+     * @param dishId
+     */
+    void updateWithFlavor(List<DishFlavor> flavors, Long dishId);
+
+    /**
+     * 单独删除菜品的口味数据
+     * @param id
+     */
+    @Delete("delete from dish_flavor where dish_id = #{id}")
+    void deleteById(Long id);
 }
