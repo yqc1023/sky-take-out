f4428c52884069354f75f959ad7a1d7399ac05bc
yqc <yqc1236@qq.com>
2024-10-16 22:13:14 +0800

    苍穹外卖 分页查询套餐和根据id查询套餐信息部分代码(业务层)

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface SetmealMapper {
+
 
     /**
      * 根据分类id查询套餐的数量
@@ -14,4 +20,13 @@ public interface SetmealMapper {
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
 
+    /**
+     * 套餐的分类查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<Setmeal> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    @Select("select * from setmeal where id = #{id}")
+    SetmealVO getById(Integer id);
 }
