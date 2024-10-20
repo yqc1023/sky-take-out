6c48d575082bfd15563d9218033defbf4b8cf6dd
yqc <yqc1236@qq.com>
2024-10-20 15:13:07 +0800

    苍穹外卖 缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -10,6 +10,9 @@ import com.sky.service.DishService;
 import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,13 +37,14 @@ public class DishController {
      * @param dishDTO
      * @return
      */
-    @PostMapping
+
+    @PostMapping                           //这里的key取值必须是与形参一致
+    //@CachePut(cacheNames = "userCache" , key = "#dishDTO.id")  //使用spring Cache缓存数据,key的生成: userCache::xxxx(1或2或3)
+    @CachePut(cacheNames = "userCache" , key = "#p0.id")//获取方法的第一个形参  清理缓存\
+
     public Result save (@RequestBody DishDTO dishDTO){
         log.info("新增菜品:{}",dishDTO);
         dishService.saveWithFlavor(dishDTO);
-        //清理缓存数据
-        String key = "dish_"+dishDTO.getCategoryId();
-        cleanCache(key);
         return Result.success();
     }
 
@@ -64,13 +68,14 @@ public class DishController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(cacheNames = "userCache" , allEntries = true) // allEntries :userCache所有的键值对都会被删除
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品批量删除:{}",ids);
         dishService.deleteBatch(ids);
 
         //清理缓存数据
         //删除多个缓存的情况则将其全部删除(先查出来再删除)
-        cleanCache("dish_*");
+        //cleanCache("dish_*");
 
         return Result.success();
     }
@@ -81,6 +86,10 @@ public class DishController {
      * @return
      */
     @GetMapping("/{id}")
+    @Cacheable(cacheNames = "userCache" , key = "#p0")
+    //spring Cache的底层是基于代理技术,加入注解后spring Cache会为当前Controller类创建一个代理对象,并先进入代理对象,在里面查询redis
+    //当代理对象在redis查到对应的缓存数据时,会直接将数据返回,不会运行本方法
+    //当代理对象在redis查不到对应的缓存数据时,CachePut底层会使用反射获取方法的返回值(dishVO),将其存入redis
     public Result<DishVO> getById(@PathVariable Long id){
         log.info("根据id查询菜品信息:{}",id);
         DishVO  dishVO = dishService.getByIdWithFlavor(id);
@@ -111,6 +120,7 @@ public class DishController {
      * @param id
      * @return
      */
+    @CacheEvict
     @PostMapping("/status/{status}")
     public Result updateStatus(@PathVariable Integer status , Long id){
         log.info("修改菜品状态:{},{}",status,id);
