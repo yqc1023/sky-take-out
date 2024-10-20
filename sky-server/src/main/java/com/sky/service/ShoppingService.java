5c903282d5179d5ebd40be450629eee96337ee6b
yqc <yqc1236@qq.com>
2024-10-20 21:49:37 +0800

    苍穹外卖 用户端购物车代码

---
+++ sky-server/src/main/java/com/sky/service/ShoppingService.java
@@ -0,0 +1,31 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
+public interface ShoppingService {
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> getShoppingCart();
+
+    /**
+     * 清空购物车
+     */
+    void cleanShoppingCart();
+
+    /**
+     * 删除购物车的一个商品
+     * @param shoppingCartDTO
+     */
+    void deleteShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
