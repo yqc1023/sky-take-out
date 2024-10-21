c74ca464e229c311dda2c6ba502691d7c9ef9cb1
yqc <yqc1236@qq.com>
2024-10-21 23:49:24 +0800

    苍穹外卖 订单管理功能代码

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,6 +13,13 @@ public interface OrderDetailMapper {
      * 插入订单明细数据
      * @param orderDetails
      */
-
     void insertBatch(List<OrderDetail> orderDetails);
+
+    /**
+     * 根据orderId查询数据
+     * @param id
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{id}")
+    List<OrderDetail> selectByOrderId(Long id);
 }
